3e1bd4bb979a69b7e1fa8ce22a795c534fdced50
Monnoroch <sidelnikov.ns@gmail.com>
2022-03-11 22:05:59 +0300

    Add basic auth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,16 +12,16 @@ import (
 )
 
 type user struct {
-	Name string `json:"name"`
+	Name     string `json:"name"`
 	Password string `json:"password"`
-
 }
 
-type config struct{
-	Port string `json:"port"`
-	Fqdn string `json:"fqdn"`
-	Basedn string`json:"basedn"`
-	Filter string`json:"filter"`
+type config struct {
+	Address string `json:"address"`
+	Port    string `json:"port"`
+	Fqdn    string `json:"fqdn"`
+	BaseDn  string `json:"basedn"`
+	Filter  string `json:"filter"`
 }
 
 var C config
@@ -34,42 +35,42 @@ var C config
 // )
 
 //Connect to LDAP server
-func Connect(c *config) (*ldap.Conn,error){
+func Connect(c *config) (*ldap.Conn, error) {
 	fmt.Println(c.Fqdn)
-    l, err := ldap.DialURL(fmt.Sprintf("ldap://%s:389", c.Fqdn))
-    if err != nil {
-        return nil, err
-    }
-    return l, nil
+	l, err := ldap.DialURL(fmt.Sprintf("ldap://%s:389", c.Fqdn))
+	if err != nil {
+		return nil, err
+	}
+	return l, nil
 }
 
 // Normal Bind and Search
-func BindAndSearch(l *ldap.Conn, c *config, u *user) (*ldap.SearchResult, error){
-	
+func BindAndSearch(l *ldap.Conn, c *config, u *user) (*ldap.SearchResult, error) {
+
 	l.Bind(u.Name, u.Password)
 
-    searchReq := ldap.NewSearchRequest(
-        c.Basedn,
-        ldap.ScopeBaseObject, // you can also use ldap.ScopeWholeSubtree
-        ldap.NeverDerefAliases,
-        0,
-        0,
-        false,
-        c.Filter,
-        []string{},
-        nil,
-    )
-	
-    result, err := l.Search(searchReq)
-    if err != nil {
-        return nil, fmt.Errorf("Search Error: %s", err)
-    }
+	searchReq := ldap.NewSearchRequest(
+		c.BaseDn,
+		ldap.ScopeBaseObject, // you can also use ldap.ScopeWholeSubtree
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		c.Filter,
+		[]string{},
+		nil,
+	)
 
-    if len(result.Entries) > 0 {
-        return result, nil
-    } else {
-        return nil, fmt.Errorf("Couldn't fetch search entries")
-    }
+	result, err := l.Search(searchReq)
+	if err != nil {
+		return nil, fmt.Errorf("Search Error: %s", err)
+	}
+
+	if len(result.Entries) > 0 {
+		return result, nil
+	} else {
+		return nil, fmt.Errorf("Couldn't fetch search entries")
+	}
 }
 
 //Reading configuration file config.json
@@ -80,20 +81,20 @@ func ReadConfig() (*config, error) {
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	err = viper.Unmarshal(&C)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	return &C, nil
 }
 
-func Process(w http.ResponseWriter, r *http.Request){
-	
-	switch r.Method{
+func Process(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
 	case "GET":
 		fmt.Fprintf(w, "Sorry! I am can read only POST request")
 	case "POST":
@@ -113,8 +114,8 @@ func Process(w http.ResponseWriter, r *http.Request){
 			break
 		}
 		defer l.Close()
-		
-		result, err := BindAndSearch(l,&C, &U)
+
+		result, err := BindAndSearch(l, &C, &U)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -133,17 +134,46 @@ func Process(w http.ResponseWriter, r *http.Request){
 
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
-	}	
+	}
 }
 
-func main(){
+func base64generate(username string, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func Basic(w http.ResponseWriter, r *http.Request) {
+
+	var U user
+	err := json.NewDecoder(r.Body).Decode(&U)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	client := http.Client{}
+	req, err := http.NewRequest("GET", C.Address, nil)
+	req.Header.Add("Authorization", "Basic "+base64generate(U.Name, U.Password))
+	_, err = client.Do(req)
+	if err != nil {
+		return
+	}
+
+	if len(req.Cookies()) < 1 {
+		fmt.Fprintf(w, "Unauthorized")
+	}
+	fmt.Fprintf(w, "OK")
+}
+
+func main() {
 	conf, err := ReadConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(conf)
 
-	http.HandleFunc("/", Process)
+	http.HandleFunc("/basic", Basic)
+	http.HandleFunc("/auth", Process)
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
